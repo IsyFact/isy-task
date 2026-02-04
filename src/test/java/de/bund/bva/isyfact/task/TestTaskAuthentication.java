0254415c9fde7323a1073d9ca100a765e87260b1
Fabian Perl <fabian.perl@seitenbau.com>
2026-02-04 08:07:25 +0100

    test: IFS-4859 fixes concurrent exception

--- src/test/java/de/bund/bva/isyfact/task/TestTaskAuthentication.java
+++ src/test/java/de/bund/bva/isyfact/task/TestTaskAuthentication.java
@@ -1,23 +1,21 @@
 package de.bund.bva.isyfact.task;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.Collections;
-
+import de.bund.bva.isyfact.task.test.TestTaskRunAssertion;
+import de.bund.bva.isyfact.task.test.config.TestConfig;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
-import de.bund.bva.isyfact.task.test.TestTaskRunAssertion;
-import de.bund.bva.isyfact.task.test.config.TestConfig;
+import java.util.Collections;
 
-import io.micrometer.core.instrument.MeterRegistry;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("security-test")
 @SpringBootTest(
@@ -29,41 +27,31 @@ import io.micrometer.core.instrument.MeterRegistry;
                 "isy.task.authentication.enabled=true",
                 "spring.task.scheduling.pool.size=2",
         })
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class TestTaskAuthentication extends AbstractOidcProviderTest {
+
+    private static final String CC_CLIENT_ID = "cc-testclient-id";
+    private static final String CC_CLIENT_SECRET = "cc-testclient-secret";
+
+    private static final String CC_INSUFFICIENT_CLIENT_ID = "cc-insufficient-testclient-id";
+    private static final String CC_INSUFFICIENT_CLIENT_SECRET = "cc-insufficient-testclient-secret";
 
     @Autowired
     private MeterRegistry registry;
-
-    @Value("${spring.security.oauth2.client.registration.cc-testclient.client-id}")
-    private String ccClientId;
-
-    @Value("${spring.security.oauth2.client.registration.cc-testclient.client-secret}")
-    private String ccClientSecret;
-
-    @Value("${spring.security.oauth2.client.registration.cc-insufficient-testclient.client-id}")
-    private String ccInsufficientClientId;
-
-    @Value("${spring.security.oauth2.client.registration.cc-insufficient-testclient.client-secret}")
-    private String ccInsufficientClientSecret;
 
     @BeforeEach
     public void setup() {
         embeddedOidcProvider.removeAllClients();
         embeddedOidcProvider.removeAllUsers();
-        embeddedOidcProvider.addClient(
-                ccClientId,
-                ccClientSecret,
-                Collections.singleton("Rolle1")
-        );
-        embeddedOidcProvider.addClient(
-                ccInsufficientClientId,
-                ccInsufficientClientSecret,
-                Collections.singleton("Rolle2")
-        );
     }
 
     @Test
     void testTaskSecured() throws Exception {
+        embeddedOidcProvider.addClient(
+                CC_CLIENT_ID,
+                CC_CLIENT_SECRET,
+                Collections.singleton("Rolle1")
+        );
         String className = TestTaskAuthenticationTasks.class.getSimpleName();
         String annotatedMethodName = "scheduledTaskSecured";
 
@@ -75,6 +63,11 @@ class TestTaskAuthentication extends AbstractOidcProviderTest {
 
     @Test
     void testTaskSecuredInsufficientRights() throws Exception {
+        embeddedOidcProvider.addClient(
+                CC_INSUFFICIENT_CLIENT_ID,
+                CC_INSUFFICIENT_CLIENT_SECRET,
+                Collections.singleton("Rolle2")
+        );
         String className = TestTaskAuthenticationTasks.class.getSimpleName();
         String annotatedMethodName = "scheduledTaskSecuredInsufficientRights";
 
@@ -85,25 +78,12 @@ class TestTaskAuthentication extends AbstractOidcProviderTest {
     }
 
     @Test
-    void testTaskSecuredNoAuthentication() throws Exception {
-        embeddedOidcProvider.removeAllClients();
-        embeddedOidcProvider.removeAllUsers();
-
-        SECONDS.sleep(5);
-
-        assertThat(TestTaskAuthenticationTasks.wasCalled.get()).isFalse();
-    }
-
-    @Test
     void testTaskSecuredMissingRoles() throws Exception {
-        embeddedOidcProvider.removeAllClients();
-        embeddedOidcProvider.removeAllUsers();
         embeddedOidcProvider.addClient(
-            ccClientId,
-            ccClientSecret,
-            Collections.emptySet()
+                CC_CLIENT_ID,
+                CC_CLIENT_SECRET,
+                Collections.emptySet()
         );
-
         String className = TestTaskAuthenticationTasks.class.getSimpleName();
         String annotatedMethodName = "scheduledTaskSecured";
 
@@ -114,4 +94,12 @@ class TestTaskAuthentication extends AbstractOidcProviderTest {
             AuthorizationDeniedException.class.getSimpleName()
         );
     }
+
+    @Test
+    void testTaskSecuredNoAuthentication() throws Exception {
+        SECONDS.sleep(5);
+
+        assertThat(TestTaskAuthenticationTasks.wasCalled.get()).isFalse();
+    }
+
 }
